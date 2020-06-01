dc6f698498f49610a706669e81689295a5d19dc1
root <root@voting-app-instance-1.us-central1-a.c.lcss-272613.internal>
2020-06-01 09:46:36 +0000

    Added cert output on index

--- election/views.py
+++ election/views.py
@@ -3,13 +3,23 @@ from django.http import HttpResponse
 from django.template.loader import *
 from django.utils.timezone import now
 
+# url decode
+from urllib.parse import unquote
+
+# decode cert
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
 
 # Database Models Import
 import election.models as electionmodels
 
 # Create your views here.
 def index(request):
+    cert_pem = unquote(request.headers["X-Client-Certificate"])
+    cert_bytes = cert_pem.encode('utf-8')
+    cert_obj = x509.load_pem_x509_certificate(cert_bytes, default_backend())
+    certificate = str(cert_obj)
     elections = electionmodels.Election.objects.filter(endDateTime__gte=now())
-    output_dict = {"elections": elections}
+    output_dict = {"elections": elections, "certificate": certificate}
     rendered = render_to_string("index.html", output_dict)
     return HttpResponse(rendered)
