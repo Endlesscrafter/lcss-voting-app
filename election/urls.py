cec87adbe5dfd805fa998d8ee4e429bc2d80129e
votingapp <votingapp@amclab.de>
2020-06-07 15:44:12 +0000

    ~ Changes to the views

--- election/urls.py
+++ election/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('election/<str:wahl>', views.election),
+    path('abgeschlossen', views.abgeschlossen)
 ]
--- election/views.py
+++ election/views.py
@@ -8,6 +8,8 @@ from urllib.parse import unquote
 
 # decode cert
 from cryptography import x509
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat._oid import ObjectIdentifier
 from cryptography.hazmat.backends import default_backend
 
 # Database Models Import
@@ -26,7 +28,51 @@ def index(request):
         cert_obj = x509.load_pem_x509_certificate(cert_bytes, default_backend())
         certificate = str(cert_obj.subject)
 
+    # Get the name of the person
+    subject = cert_obj.subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
+
+    certificate = cert_obj.subject.get_attributes_for_oid(NameOID.POSTAL_CODE)#[0].value
+
     elections = electionmodels.Election.objects.filter(endDateTime__gte=now())
-    output_dict = {"elections": elections, "certificate": certificate}
+
+    output_dict = {"elections": elections, "certificate": certificate, "name": subject}
     rendered = render_to_string("index.html", output_dict)
+    return HttpResponse(rendered)
+
+def election(request, wahl="empty"):
+    try:
+    	cert_urlencoded = request.headers["X-Client-Certificate"]
+    except KeyError:
+        cert_urlencoded = None
+    certificate = ""
+    if (cert_urlencoded != None):
+        cert_pem = unquote(cert_urlencoded)
+        cert_bytes = cert_pem.encode('utf-8')
+        cert_obj = x509.load_pem_x509_certificate(cert_bytes, default_backend())
+        certificate = str(cert_obj.subject)
+
+    # Get the name of the person
+    subject = cert_obj.subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
+
+    output_dict = {"name": subject}
+    rendered = render_to_string("abstimmung.html", output_dict)
+    return HttpResponse(rendered)
+
+def abgeschlossen(request):
+    try:
+    	cert_urlencoded = request.headers["X-Client-Certificate"]
+    except KeyError:
+        cert_urlencoded = None
+    certificate = ""
+    if (cert_urlencoded != None):
+        cert_pem = unquote(cert_urlencoded)
+        cert_bytes = cert_pem.encode('utf-8')
+        cert_obj = x509.load_pem_x509_certificate(cert_bytes, default_backend())
+        certificate = str(cert_obj.subject)
+
+    # Get the name of the person
+    subject = cert_obj.subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
+
+    output_dict = {"name": subject}
+    rendered = render_to_string("abgeschlosseneWahl.html", output_dict)
     return HttpResponse(rendered)
