ad8a157faa41d93867764de3812463a034346a79
votingapp <votingapp@amclab.de>
2020-06-07 14:17:33 +0000

    + Test
    Signed-off-by: votingapp <votingapp@amclab.de>

--- election/views.py
+++ election/views.py
@@ -15,10 +15,17 @@ import election.models as electionmodels
 
 # Create your views here.
 def index(request):
-    cert_pem = unquote(request.headers["X-Client-Certificate"])
-    cert_bytes = cert_pem.encode('utf-8')
-    cert_obj = x509.load_pem_x509_certificate(cert_bytes, default_backend())
-    certificate = str(cert_obj)
+    try:
+    	cert_urlencoded = request.headers["X-Client-Certificate"]
+    except KeyError:
+        cert_urlencoded = None
+    certificate = ""
+    if (cert_urlencoded != None):
+        cert_pem = unquote(cert_urlencoded)
+        cert_bytes = cert_pem.encode('utf-8')
+        cert_obj = x509.load_pem_x509_certificate(cert_bytes, default_backend())
+        certificate = str(cert_obj.subject)
+
     elections = electionmodels.Election.objects.filter(endDateTime__gte=now())
     output_dict = {"elections": elections, "certificate": certificate}
     rendered = render_to_string("index.html", output_dict)
